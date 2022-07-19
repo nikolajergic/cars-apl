564774e2d54852d537c3d902ce3a684eb832a590
unknown <Nikola Jeric>
2022-07-19 11:31:59 +0200

    Prvi zadatak

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import AppCars from './pages/AppCars';
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/cars'>Cars</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/cars'>
+            <AppCars />
+          </Route>
+        </Switch>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AppCars.js
@@ -0,0 +1,5 @@
+ function AppCars() {
+    
+}
+
+export default AppCars;
